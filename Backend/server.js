75e789b3728a2b6772f382c00773cac407662ee2
saschafrentzen <sascha.frentzen@tn.techstarter.de>
2025-03-05 12:54:37 +0100

    datenbank erstellt ,post ,db.run

--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,18 @@
 const express = require('express');
-
+const sqlite3 = require('sqlite3');
+const bodyParser = require('body-parser');
 const app = express();
 
 //TODO: Verbinde eine Datenbank dazu
+
+const db = new sqlite3.Database('./tasks.db');
+
+app.use(bodyParser.json());   // Middleware
+
+db.run('CREATE TABLE IF NOT EXISTS tasks (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, completed BOOLEAN DEFAULT 0)');
+
+
+db.run('INSERT INTO tasks (title) VALUES (?)', "Zähne putzen");
 
 //TODU: Schreibe request/responses
 
@@ -13,6 +23,12 @@ app.get('/', (req, res) => {
 
 app.get('/', (req, res) => {
     res.send('Vielen dank Sascha');
+});
+
+app.post('/add', (req, res) => {
+    db.run('INSERT INTO tasks (title) VALUES (?)', [req.body.title], function (){
+    res.json({tag: "Mittwoch", bald_wirds: "Mittagspause"});
+    });
 });
 
 app.listen(3050, "localhost", () => {
